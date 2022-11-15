d2a9faaba478e4dc246ab09d0050011535faee75
Chizoba Victory <chizoba.victory1@gmail.com>
2022-11-15 21:24:06 +0100

    commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ document.getElementById("bored-bot").addEventListener("click", function () {
   fetch("https://apis.scrimba.com/bored/api/activity")
     .then((response) => response.json())
     .then((data) => {
+      document.getElementById("title").textContent = "🦾 HappyBot🦿";
       document.getElementById("random-activities").textContent = data.activity;
     });
 
   fetch("https://dog.ceo/api/breeds/image/random")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       document.getElementById("image-container").innerHTML = `
   <img src="${data.message}"  /> `;
     });
