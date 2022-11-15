a07c41ebe127e720bac8757638e88fa86f873495
Chizoba Victory <chizoba.victory1@gmail.com>
2022-11-15 21:16:54 +0100

    comit third

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    document.getElementById("image-container").innerHTML = `
-  <img src="${data.message}" /> `;
-  });
+document.getElementById("bored-bot").addEventListener("click", function () {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("random-activities").textContent = data.activity;
+    });
 
-fetch("https://apis.scrimba.com/bored/api/activity")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("random-activities").textContent = data.activity;
-  });
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("image-container").innerHTML = `
+  <img src="${data.message}"  /> `;
+    });
+});
